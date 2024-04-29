425e0b2808def71da6efdc8bf351f28a388b7b63
Miriam Wanjohi <njoks2@gmail.com>
2024-04-29 10:18:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
